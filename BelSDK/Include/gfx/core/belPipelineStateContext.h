6bd6762d6cccc89336fe3cdeac5c2e4ba2c1b8fe
belmayze <belmayze@hotmail.co.jp>
2023-11-26 16:41:07 +0900

    パイプラインステートを外から設定できるようにする

---
+++ BelSDK/Include/gfx/core/belPipelineStateContext.h
@@ -0,0 +1,176 @@
+﻿/*!
+ * @file   belPipelineStateContext.h
+ * @brief  
+ * @author belmayze
+ * 
+ * Copyright (c) belmayze. All rights reserved.
+ */
+#pragma once
+ // C++
+#include <array>
+#define NOMINMAX
+#include <d3d12.h>
+
+namespace bel::gfx
+{
+
+//-----------------------------------------------------------------------------
+/*!
+ * パイプラインステート列挙
+ */
+
+//-----------------------------------------------------------------------------
+/*!
+ * パイプラインステート
+ */
+class PipelineStateContext
+{
+    //-------------------------------------------------------------------------
+    // 定数
+    //-------------------------------------------------------------------------
+public:
+    static constexpr uint32_t cMaxRenderTarget = 8;
+
+    //-------------------------------------------------------------------------
+    // enum
+    //-------------------------------------------------------------------------
+public:
+    //! デプス比較式
+    enum DepthFunc
+    {
+        cDepthFuncNever        = D3D12_COMPARISON_FUNC_NEVER,
+        cDepthFuncLess         = D3D12_COMPARISON_FUNC_LESS,
+        cDepthFuncEqual        = D3D12_COMPARISON_FUNC_EQUAL,
+        cDepthFuncLessEqual    = D3D12_COMPARISON_FUNC_LESS_EQUAL,
+        cDepthFuncGreater      = D3D12_COMPARISON_FUNC_GREATER,
+        cDepthFuncNotEqual     = D3D12_COMPARISON_FUNC_NOT_EQUAL,
+        cDepthFuncGreaterEqual = D3D12_COMPARISON_FUNC_GREATER_EQUAL,
+        cDepthFuncAlways       = D3D12_COMPARISON_FUNC_ALWAYS
+    };
+    //! ブレンド値
+    enum BlendFactor
+    {
+        cBlendFactorZero         = D3D12_BLEND_ZERO,
+        cBlendFactorOne          = D3D12_BLEND_ONE,
+        cBlendFactorSrcColor     = D3D12_BLEND_SRC_COLOR,
+        cBlendFactorInvSrcColor  = D3D12_BLEND_INV_SRC_COLOR,
+        cBlendFactorSrcAlpha     = D3D12_BLEND_SRC_ALPHA,
+        cBlendFactorInvSrcAlpha  = D3D12_BLEND_INV_SRC_ALPHA,
+        cBlendFactorDestAlpha    = D3D12_BLEND_DEST_ALPHA,
+        cBlendFactorInvDestAlpha = D3D12_BLEND_INV_DEST_ALPHA,
+        cBlendFactorDestColor    = D3D12_BLEND_DEST_COLOR,
+        cBlendFactorInvDestColor = D3D12_BLEND_INV_DEST_COLOR
+    };
+    //! ブレンド式
+    enum BlendOp
+    {
+        cBlendOpAdd         = D3D12_BLEND_OP_ADD,
+        cBlendOpSubtract    = D3D12_BLEND_OP_SUBTRACT,
+        cBlendOpRevSubtract = D3D12_BLEND_OP_REV_SUBTRACT,
+        cBlendOpMin         = D3D12_BLEND_OP_MIN,
+        cBlendOpMax         = D3D12_BLEND_OP_MAX
+    };
+
+    //-------------------------------------------------------------------------
+public:
+    //! ブレンド設定
+    struct RenderTargetBlendDesc
+    {
+        bool        mEnable    = false;
+        uint8_t     mWriteMask = 0xF;
+        BlendFactor mSrcColor  = cBlendFactorSrcAlpha;
+        BlendFactor mDstColor  = cBlendFactorInvSrcAlpha;
+        BlendOp     mColorOp   = cBlendOpAdd;
+        BlendFactor mSrcAlpha  = cBlendFactorOne;
+        BlendFactor mDstAlpha  = cBlendFactorOne;
+        BlendOp     mAlphaOp   = cBlendOpAdd;
+    };
+    struct BlendDesc
+    {
+        std::array<RenderTargetBlendDesc, cMaxRenderTarget> mRenderTargets;
+    };
+
+    //! デプス設定
+    struct DepthStencilDesc
+    {
+        bool      mEnableDepth   = false;
+        bool      mDepthWrite    = false;
+        DepthFunc mDepthFunc     = cDepthFuncLess;
+        bool      mEnableStencil = false;
+    };
+
+    //-------------------------------------------------------------------------
+    // Mask
+    //-------------------------------------------------------------------------
+public:
+    /*!
+     * 出力カラーマスク
+     * @param[in] r     trueの場合、Rチャンネルの出力を行います
+     * @param[in] g     trueの場合、Gチャンネルの出力を行います
+     * @param[in] b     trueの場合、Bチャンネルの出力を行います
+     * @param[in] a     trueの場合、Aチャンネルの出力を行います
+     * @param[in] index レンダリングターゲット番号
+     */
+    void setColorMask(bool r, bool g, bool b, bool a, uint32_t index);
+
+    //-------------------------------------------------------------------------
+    // Depth
+    //-------------------------------------------------------------------------
+public:
+    /*!
+     * デプステストおよびデプス書き込み
+     * @param[in] enable trueの場合、デプステスト有効
+     * @param[in] write  trueの場合、デプス書き込み有効
+     */
+    void setDepthTest(bool enable, bool write);
+
+    /*!
+     * デプステストの評価方法変更
+     * @param[in] func 計算式
+     */
+    void setDepthFunc(DepthFunc func);
+
+    //-------------------------------------------------------------------------
+    // Blend
+    //-------------------------------------------------------------------------
+public:
+    /*!
+     * ブレンドの有効設定
+     * @param[in] enable trueの場合、ブレンド有効
+     * @param[in] index  レンダリングターゲット番号
+     */
+    void setBlendEnable(bool enable, uint32_t index);
+
+    /*!
+     * ブレンド計算式
+     * @param[in] src_color
+     * @param[in] dst_color
+     * @param[in] color_op
+     * @param[in] src_alpha
+     * @param[in] dst_alpha
+     * @param[in] alpha_op
+     * @param[in] index  レンダリングターゲット番号
+     */
+    void setBlendFunc(
+        BlendFactor src_color, BlendFactor dst_color, BlendOp color_op,
+        BlendFactor src_alpha, BlendFactor dst_alpha, BlendOp alpha_op,
+        uint32_t index
+    );
+
+    //-------------------------------------------------------------------------
+    // getter
+    //-------------------------------------------------------------------------
+public:
+    //! BlendDesc
+    const BlendDesc& getBlendDesc() const { return mBlendDesc; }
+    //! DepthStencilDesc
+    const DepthStencilDesc& getDepthStencilDesc() const { return mDepthStencilDesc; }
+
+    //-------------------------------------------------------------------------
+private:
+    BlendDesc        mBlendDesc;
+    DepthStencilDesc mDepthStencilDesc;
+};
+//-----------------------------------------------------------------------------
+
+}
--- BelSDK/Source/gfx/core/belPipelineState.cpp
+++ BelSDK/Source/gfx/core/belPipelineState.cpp
@@ -34,14 +34,28 @@ bool PipelineState::initialize(const InitializeArg& arg)
 
         // ブレンダー
         D3D12_BLEND_DESC blend_desc = {};
-        blend_desc.RenderTarget[0].BlendEnable           = false;
-        blend_desc.RenderTarget[0].RenderTargetWriteMask = D3D12_COLOR_WRITE_ENABLE_ALL;
+        {
+            const PipelineStateContext::BlendDesc& desc = arg.mContext.getBlendDesc();
+
+            for (uint32_t i = 0; i < PipelineStateContext::cMaxRenderTarget; ++i)
+            {
+                blend_desc.RenderTarget[i].BlendEnable           = desc.mRenderTargets[i].mEnable;
+                blend_desc.RenderTarget[i].RenderTargetWriteMask = desc.mRenderTargets[i].mWriteMask;
+                blend_desc.RenderTarget[i].SrcBlend              = static_cast<D3D12_BLEND>(desc.mRenderTargets[i].mSrcColor);
+                blend_desc.RenderTarget[i].DestBlend             = static_cast<D3D12_BLEND>(desc.mRenderTargets[i].mDstColor);
+                blend_desc.RenderTarget[i].BlendOp               = static_cast<D3D12_BLEND_OP>(desc.mRenderTargets[i].mColorOp);
+                blend_desc.RenderTarget[i].SrcBlendAlpha         = static_cast<D3D12_BLEND>(desc.mRenderTargets[i].mSrcAlpha);
+                blend_desc.RenderTarget[i].DestBlendAlpha        = static_cast<D3D12_BLEND>(desc.mRenderTargets[i].mDstAlpha);
+                blend_desc.RenderTarget[i].BlendOpAlpha          = static_cast<D3D12_BLEND_OP>(desc.mRenderTargets[i].mAlphaOp);
+
+            }
+        }
 
         // デプステスト
         D3D12_DEPTH_STENCIL_DESC depth_stencil_desc = {};
-        depth_stencil_desc.DepthEnable    = false;
-        depth_stencil_desc.DepthWriteMask = D3D12_DEPTH_WRITE_MASK_ALL;
-        depth_stencil_desc.DepthFunc      = D3D12_COMPARISON_FUNC_LESS;
+        depth_stencil_desc.DepthEnable    = arg.mContext.getDepthStencilDesc().mEnableDepth;
+        depth_stencil_desc.DepthWriteMask = arg.mContext.getDepthStencilDesc().mDepthWrite ? D3D12_DEPTH_WRITE_MASK_ALL : D3D12_DEPTH_WRITE_MASK_ZERO;
+        depth_stencil_desc.DepthFunc      = static_cast<D3D12_COMPARISON_FUNC>(arg.mContext.getDepthStencilDesc().mDepthFunc);
 
         // パイプライン設定
         if (shader_mode == Shader::cModeVertexPixel)
@@ -54,16 +68,31 @@ bool PipelineState::initialize(const InitializeArg& arg)
             // RootSignature
             desc.pRootSignature = &RootSignature::GetInstance().getGraphicsRootSignature();
 
-            // @TODO: 頂点レイアウト
-            desc.InputLayout;
+            // 頂点レイアウト
+            std::array<D3D12_INPUT_ELEMENT_DESC, cMaxVertexAttribute> input_elements;
+            for (uint32_t i = 0; i < arg.mInputContext.mNumInputLayout; ++i)
+            {
+                input_elements[i].SemanticName         = arg.mInputContext.mInputLayouts[i].mName.c_str();
+                input_elements[i].SemanticIndex        = arg.mInputContext.mInputLayouts[i].mSemanticIndex;
+                input_elements[i].Format               = arg.mInputContext.mInputLayouts[i].mFormat;
+                input_elements[i].InputSlot            = arg.mInputContext.mInputLayouts[i].mSlotIndex;
+                input_elements[i].AlignedByteOffset    = arg.mInputContext.mInputLayouts[i].mOffset;
+                input_elements[i].InstanceDataStepRate = arg.mInputContext.mInputLayouts[i].mInstanceDataStepRate;
+                input_elements[i].InputSlotClass       = input_elements[i].InstanceDataStepRate == 0 ? D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA : D3D12_INPUT_CLASSIFICATION_PER_INSTANCE_DATA;
+            }
+            desc.InputLayout.pInputElementDescs = input_elements.data();
+            desc.InputLayout.NumElements        = arg.mInputContext.mNumInputLayout;
 
-            // @TODO: トポロジー
-            desc.PrimitiveTopologyType;
+            // トポロジー
+            desc.PrimitiveTopologyType = arg.mInputContext.mPrimitiveTopology;
 
-            // @TODO: 出力フォーマット
-            desc.NumRenderTargets;
-            desc.RTVFormats[0];
-            desc.DSVFormat;
+            // 出力フォーマット
+            desc.NumRenderTargets = arg.mOutputContext.mNumRenderTarget;
+            for (uint32_t i = 0; i < arg.mOutputContext.mNumRenderTarget; ++i)
+            {
+                desc.RTVFormats[i] = arg.mOutputContext.mColorFormats[i];
+            }
+            desc.DSVFormat = arg.mOutputContext.mDepthFormat;
 
             // シェーダー
             desc.VS.pShaderBytecode = vs_ps.mVertexShaderPtr;
