0a67936d5307ea02a5b7f6a6373420b124260aa1
belmayze <twitter_it@belmayze.jp>
2025-05-03 14:58:47 +0900

    最低限のリスト実装

--- BelSDK/Include/container/belList.h
+++ BelSDK/Include/container/belList.h
@@ -21,7 +21,7 @@ public:
     //! コンストラクター
     List() {}
     List(size_t num)
-        : mBuffer(std::make_unique<uint8_t[]>(sizeof(list_node)* num))
+        : mBuffer(std::make_unique<uint8_t[]>(sizeof(list_node) * ( num + 1 )))
         , mFreeBuffer(num)
         , mMaxSize(num)
     {
@@ -30,6 +30,11 @@ public:
         {
             mFreeBuffer.push_back(&get_(i));
         }
+
+        // 最後は end 用のバッファー
+        mpHead = &get_(num);
+        mpHead->next = mpHead;
+        mpHead->prev = mpHead;
     }
 
     //! デストラクター
@@ -57,7 +62,10 @@ public:
         friend class List<T>;
 
     public:
+        using iterator_category = std::bidirectional_iterator_tag;
+
         using value_type = T;
+        using difference_type = std::ptrdiff_t;
 
     public:
         const_iterator(list_node* p_node, const List<T>* p_instance) : mpInstance(p_instance), mpNode(p_node) {}
@@ -67,11 +75,41 @@ public:
 
         const T& operator*() const { return mpNode->instance; }
         const T* operator->() const { return &mpNode->instance; }
+        bool operator==(const const_iterator& rhs) const { BEL_ASSERT(mpInstance == rhs.mpInstance); return mpNode == rhs.mpNode; }
 
-    private:
+        const_iterator& operator++() { mpNode = mpNode->next; return *this; }
+        const_iterator operator++(int) { const_iterator tmp = *this; ++*this; return tmp; }
+        const_iterator& operator--() { mpNode = mpNode->prev; return *this; }
+        const_iterator operator--(int) { const_iterator tmp = *this; --*this; return tmp; }
+
+    protected:
         const List<T>* mpInstance = nullptr;
         list_node* mpNode;
     };
+
+    //! iterator
+    class iterator : public const_iterator
+    {
+        friend class List<T>;
+        using Base = const_iterator;
+
+    public:
+        using value_type = T;
+
+    public:
+        using Base::Base;
+
+        T& operator*() const { return const_cast<T&>(Base::operator*()); }
+        T* operator->() const { return const_cast<T*>(Base::operator->()); }
+
+        iterator& operator++() { Base::operator++(); return *this; }
+        iterator operator++(int) { iterator tmp = *this; Base::operator++(); return tmp; }
+        iterator& operator--() { Base::operator--(); return *this; }
+        iterator operator--(int) { iterator tmp = *this; Base::operator--(); return tmp; }
+    };
+
+    using reverse_iterator = std::reverse_iterator<iterator>;
+    using const_reverse_iterator = std::reverse_iterator<const_iterator>;
 
     //-------------------------------------------------------------------------
     // operation
@@ -84,33 +122,31 @@ public:
     template <class... Args>
     T& emplace_back(Args&&... args)
     {
+        return *emplace(end(), std::forward<Args>(args)...);
+    }
+
+    //! 任意のイテレーターの前に要素を追加する
+    template <class... Args>
+    iterator emplace(const_iterator position, Args&&... args)
+    {
         // 空きコンテナ取得
         list_node& node = *mFreeBuffer.at(mFreeBuffer.size() - 1);
         mFreeBuffer.pop_back();
+        BEL_ASSERT(node.next == nullptr && node.prev == nullptr);
 
         // ポインター接続
-        if (mpHead)
-        {
-            node.next = mpHead;
-            node.prev = mpHead->prev;
+        node.next = position.mpNode;
+        node.prev = position.mpNode->prev;
 
-            mpHead->prev->next = &node;
-            mpHead->prev = &node;
-        }
-        else
-        {
-            node.next = &node;
-            node.prev = &node;
-
-            mpHead = &node;
-        }
+        node.next->prev = &node;
+        node.prev->next = &node;
 
         // 加算
         mSize++;
 
         // メモリー確保
         T* p = new (&node.instance) T(std::forward<Args>(args)...);
-        return *p;
+        return iterator(&node, this);
     }
 
     //! 任意の要素を削除する
@@ -119,19 +155,12 @@ public:
         // belList で確保された T であれば、このアドレスの前にポインターが入っている
         uint8_t* p = reinterpret_cast<uint8_t*>(&x);
         list_node* node = reinterpret_cast<list_node*>(p - sizeof(list_node*) * 2);
+        BEL_ASSERT(node->next != nullptr && node->prev != nullptr);
 
         // 接続を切る
-        if (mSize == 1)
-        {
-            // 最後ならすべて削除
-            mpHead = nullptr;
-        }
-        else
-        {
-            node->next->prev = node->prev;
-            node->prev->next = node->next;
-            if (mpHead == node) { mpHead = node->next; }
-        }
+        node->next->prev = node->prev;
+        node->prev->next = node->next;
+
         node->next = nullptr;
         node->prev = nullptr;
 
@@ -142,19 +171,36 @@ public:
         // フリーへ
         mFreeBuffer.push_back(node);
     }
-    void erase(const_iterator it)
+    iterator erase(iterator it)
     {
-
+        iterator tmp = it;
+        ++tmp;
         erase(it.mpNode->instance);
+        return tmp;
+    }
+    const_iterator erase(const_iterator it)
+    {
+        const_iterator tmp = it;
+        ++tmp;
+        erase(it.mpNode->instance);
+        return tmp;
     }
 
     // 要素をすべて削除する
-    void clear() { while (!empty()) { erase(mpHead->instance); } }
+    void clear() { while (!empty()) { erase(begin()); } }
 
     //-------------------------------------------------------------------------
     // getter
     //-------------------------------------------------------------------------
 public:
+    //! 先頭の要素を取得する
+    T& front() { BEL_ASSERT(!empty()); return mpHead->next->instance; }
+    const T& front() const { BEL_ASSERT(!empty()); return mpHead->next->instance; }
+
+    //! 末尾の要素を取得する
+    T& back() { BEL_ASSERT(!empty()); return mpHead->prev->instance; }
+    const T& back()const { BEL_ASSERT(!empty()); return mpHead->prev->instance; }
+
     //! 要素数を取得する
     size_t size() const { return mSize; }
 
@@ -165,15 +211,32 @@ public:
     size_t max_size() const { return mMaxSize; }
 
     //! 先頭要素を指すイテレーターを取得する
-    const_iterator begin() const { return const_iterator(mpHead, this); }
+    iterator begin() { return iterator(mpHead->next, this); }
+    const_iterator begin() const { return const_iterator(mpHead->next, this); }
+    const_iterator cbegin() const { return begin(); }
+
+    //! 末尾要素を指すイテレーターを取得する
+    iterator end() { return iterator(mpHead, this); }
+    const_iterator end() const { return const_iterator(mpHead, this); }
+    const_iterator cend() const { return end(); }
+
+    //! 末尾要素を指す逆イテレーターを取得する
+    reverse_iterator rbegin() { return reverse_iterator(end()); }
+    const_reverse_iterator rbegin() const { return const_reverse_iterator(end()); }
+    const_reverse_iterator crbegin() const { return rbegin(); }
+
+    //! 先頭要素を指す逆イテレーターを取得する
+    reverse_iterator rend() { return reverse_iterator(begin()); }
+    const_reverse_iterator rend() const { return const_reverse_iterator(begin()); }
+    const_reverse_iterator crend() const { return rend(); }
 
     //-------------------------------------------------------------------------
 private:
     std::unique_ptr<uint8_t[]> mBuffer;
     Vector<list_node*>         mFreeBuffer;
     size_t     mMaxSize = 0;
-    size_t     mSize    = 0;
-    list_node* mpHead  = nullptr;
+    size_t     mSize = 0;
+    list_node* mpHead = nullptr;
 
     //-------------------------------------------------------------------------
     // internal
--- BelSDK/Test/Source/test/container/testList.cpp
+++ BelSDK/Test/Source/test/container/testList.cpp
@@ -41,35 +41,35 @@ void List::test()
     buffer.push_back(0);
     buffer.emplace_back(1);
     buffer.push_back(2);
-    //BEL_ASSERT(buffer[0] == 0 && buffer.at(1) == 1 && buffer[2] == 2);
+    BEL_ASSERT(*buffer.begin() == 0 && *std::next(buffer.begin(), 1) == 1 && *std::next(buffer.begin(), 2) == 2);
     BEL_ASSERT(buffer.size() == 3);
 
     // 削除チェック
+    buffer.erase(std::prev(buffer.end()));
     buffer.erase(buffer.begin());
-    buffer.erase(buffer.begin());
-    //BEL_ASSERT(buffer[0] == 0);
+    BEL_ASSERT(*buffer.begin() == 1);
     BEL_ASSERT(buffer.size() == 1);
 
     // 循環チェック
     buffer.push_back(3);
     buffer.push_back(4);
-    //BEL_ASSERT(buffer[0] == 0 && buffer[1] == 3 && buffer[2] == 4);
+    BEL_ASSERT(*buffer.begin() == 1 && *std::next(buffer.begin(), 1) == 3 && *std::next(buffer.begin(), 2) == 4);
     BEL_ASSERT(buffer.size() == 3);
 
     // イテレーター
     BEL_DEBUG_LOG("  iterator[");
-    //for (auto& c : buffer)
-    //{
-    //    BEL_DEBUG_LOG("%d, ", c);
-    //}
+    for (auto& c : buffer)
+    {
+        BEL_DEBUG_LOG("%d, ", c);
+    }
     BEL_DEBUG_LOG("] (size: %zu)\n", buffer.size());
 
     // イテレーター
     BEL_DEBUG_LOG("  reverse iterator[");
-    //for (auto it = buffer.crbegin(); it != buffer.crend(); ++it)
-    //{
-    //    BEL_DEBUG_LOG("%d, ", *it);
-    //}
+    for (auto it = buffer.crbegin(); it != buffer.crend(); ++it)
+    {
+        BEL_DEBUG_LOG("%d, ", *it);
+    }
     BEL_DEBUG_LOG("] (size: %zu)\n", buffer.size());
 
     // クリア
