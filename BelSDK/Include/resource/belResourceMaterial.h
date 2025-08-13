20b4570a34e825417d196af98cd57677675f4aee
belmayze <twitter_it@belmayze.jp>
2025-08-13 13:39:21 +0900

    メッシュとマテリアルのファイル構造仮

--- BelSDK/Include/resource/belResourceMaterial.h
+++ BelSDK/Include/resource/belResourceMaterial.h
@@ -20,17 +20,151 @@ class Material
 public:
     //! 無効状態で初期化
     Material() {}
+    //! 先頭アドレスで初期化
+    Material(uint8_t* ptr) : mPtr(ptr) {}
 
     //-------------------------------------------------------------------------
     // 情報取得
     //-------------------------------------------------------------------------
 public:
     //! 使用するメッシュ番号
-    uint32_t getMeshIndex() const { return 0; }
+    uint32_t getMeshIndex() const
+    {
+        return getFileHeader_().mesh_index;
+    }
     //! 使用するメッシュ開始インデックス
-    size_t getMeshStartIndex() const { return 0; }
+    size_t getMeshStartIndex() const
+    {
+        return getFileHeader_().mesh_start_index;
+    }
     //! 使用するメッシュインデックス数
-    size_t getMeshIndexCount() const { return std::numeric_limits<uint32_t>().max(); }
+    size_t getMeshIndexCount() const
+    {
+        return getFileHeader_().mesh_index_count;
+    }
+
+    //! テクスチャー数を取得
+    uint32_t getNumTexture() const
+    {
+        return getFileHeader_().num_texture;
+    }
+    //! テクスチャー名を取得
+    const char* getTextureName(uint32_t index) const
+    {
+        const TextureInfo& texture_info = getTextureInfo_(index);
+        // データが改変され、終端文字が消えても大丈夫か？
+        return getStringPtr_(texture_info.name_offset, texture_info.name_strlen);
+    }
+
+    //! サンプラー数を取得
+    uint32_t getNumSampler() const
+    {
+        return getFileHeader_().num_sampler;
+    }
+    //! サンプラー
+    void getSampler(D3D12_SAMPLER_DESC& out, uint32_t index) const
+    {
+        const SamplerInfo& sampler_info = getSamplerInfo_(index);
+
+        out.Filter        = static_cast<D3D12_FILTER>(sampler_info.filter);
+        out.AddressU      = static_cast<D3D12_TEXTURE_ADDRESS_MODE>(sampler_info.address_u);
+        out.AddressV      = static_cast<D3D12_TEXTURE_ADDRESS_MODE>(sampler_info.address_v);
+        out.AddressW      = static_cast<D3D12_TEXTURE_ADDRESS_MODE>(sampler_info.address_w);
+        out.MipLODBias    = sampler_info.mip_lod_bias;
+        out.MaxAnisotropy = sampler_info.anisotropy;
+        std::memcpy(out.BorderColor, sampler_info.border_color, sizeof(float) * 4);
+        out.MinLOD = sampler_info.min_mip_lod;
+        out.MaxLOD = sampler_info.max_mip_lod;
+    }
+
+    //-------------------------------------------------------------------------
+    // ファイルフォーマット
+    // 
+    // +------------------+--------------+
+    // | Header           | 12 bytes     |
+    // +------------------+--------------+
+    // | Textures         |  8 bytes * N |
+    // +------------------+--------------+
+    // | Samplers         | 36 bytes * N |
+    // +------------------+--------------+
+    // | Strings          | variable     |
+    // +------------------+--------------+
+    // 
+    //-------------------------------------------------------------------------
+private:
+    //! ヘッダー構造体
+    struct FileHeader
+    {
+        uint8_t  mesh_index         = 0;
+        uint8_t  num_sampler        = 0;
+        uint8_t  num_texture        = 0;
+        uint8_t  shader_name_strlen = 0;
+        uint32_t mesh_start_index   = 0;
+        uint32_t mesh_index_count   = 0;
+        uint32_t shader_name_offset = 0;
+    };
+    static_assert(sizeof(FileHeader) == 16);
+
+    // テクスチャー情報
+    struct TextureInfo
+    {
+        uint8_t  name_strlen = 0;
+        uint32_t name_offset = 0;
+    };
+    static_assert(sizeof(TextureInfo) == 8);
+
+    // テクスチャー情報
+    struct SamplerInfo
+    {
+        uint8_t filter            = D3D12_FILTER_MIN_MAG_MIP_LINEAR;
+        uint8_t address_u         = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
+        uint8_t address_v         = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
+        uint8_t address_w         = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
+        uint8_t shader_visibility = D3D12_SHADER_VISIBILITY_ALL;
+        uint8_t anisotropy        = 0;
+        float   max_mip_lod       = std::numeric_limits<float>::max();
+        float   min_mip_lod       = 0.f;
+        float   mip_lod_bias      = 0.f;
+        float   border_color[4] = { 0.f };
+    };
+    static_assert(sizeof(SamplerInfo) == 36);
+
+    //-------------------------------------------------------------------------
+private:
+    uint8_t* mPtr = nullptr;
+
+    //-------------------------------------------------------------------------
+    // internal function
+    //-------------------------------------------------------------------------
+private:
+    //! ファイルヘッダーを取得
+    const FileHeader& getFileHeader_() const
+    {
+        BEL_ASSERT(mPtr != nullptr);
+        return *reinterpret_cast<const FileHeader*>(mPtr);
+    }
+    //! テクスチャー情報を取得
+    const TextureInfo& getTextureInfo_(uint32_t index) const
+    {
+        BEL_ASSERT(mPtr != nullptr);
+        BEL_ASSERT(index < getFileHeader_().num_texture);
+        return reinterpret_cast<const TextureInfo*>(mPtr + sizeof(FileHeader))[index];
+    }
+    //! サンプラー情報を取得
+    const SamplerInfo& getSamplerInfo_(uint32_t index) const
+    {
+        BEL_ASSERT(mPtr != nullptr);
+        const FileHeader& file_header = getFileHeader_();
+        BEL_ASSERT(index < file_header.num_sampler);
+        size_t offset = sizeof(FileHeader) + sizeof(TextureInfo) * file_header.num_texture;
+        return reinterpret_cast<const SamplerInfo*>(mPtr + offset)[index];
+    }
+    //! 文字列取得
+    const char* getStringPtr_(size_t offset, size_t strlen) const
+    {
+        BEL_ASSERT(mPtr != nullptr);
+        return reinterpret_cast<const char*>(mPtr + offset);
+    }
 };
 
 //-----------------------------------------------------------------------------
--- BelSDK/Include/resource/belResourceMesh.h
+++ BelSDK/Include/resource/belResourceMesh.h
@@ -20,31 +20,161 @@ class Mesh
 public:
     //! 無効状態で初期化
     Mesh() {}
+    //! 先頭アドレスで初期化
+    Mesh(uint8_t* ptr) : mPtr(ptr) {}
 
     //-------------------------------------------------------------------------
     // 情報取得
     //-------------------------------------------------------------------------
 public:
     //! 頂点バッファー
-    void* getVertexBuffer() const { return nullptr; }
+    void* getVertexBuffer() const
+    {
+        return getVertexBufferPtr_();
+    }
     //! 頂点バッファーサイズ
-    size_t getVertexBufferSize() const { return 0; }
+    size_t getVertexBufferSize() const
+    {
+        return getFileHeader_().vertex_buffer_size;
+    }
     //! 頂点バッファーストライドサイズ
-    size_t getVertexStride() const { return 0; }
+    size_t getVertexStride() const
+    {
+        return getFileHeader_().vertex_stride_size;
+    }
 
     //! インデックスバッファー
-    void* getIndexBuffer() const { return nullptr; }
+    void* getIndexBuffer() const
+    {
+        return getIndexBufferPtr_();
+    }
     //! インデックスバッファーサイズ
-    size_t getIndexBufferSize() const { return 0; }
+    size_t getIndexBufferSize() const
+    {
+        return getFileHeader_().index_buffer_size;
+    }
     //! インデックスバッファーフォーマット
-    DXGI_FORMAT getIndexBufferFormat() const { return DXGI_FORMAT_R16_UINT; }
+    DXGI_FORMAT getIndexBufferFormat() const
+    {
+        return static_cast<DXGI_FORMAT>(getFileHeader_().index_buffer_format);
+    }
 
     //! トポロジー
-    D3D_PRIMITIVE_TOPOLOGY getPrimitiveTopology() const { return D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST; }
+    D3D_PRIMITIVE_TOPOLOGY getPrimitiveTopology() const
+    {
+        return static_cast<D3D_PRIMITIVE_TOPOLOGY>(getFileHeader_().primitive_topology);
+    }
+
+    //! 頂点レイアウトの個数
+    uint32_t getNumInputLayout() const
+    {
+        return getFileHeader_().num_input_layout;
+    }
+    //! 頂点レイアウト
+    void getInputLayout(D3D12_INPUT_ELEMENT_DESC& out, uint32_t index) const
+    {
+        const InputLayout& layout = getInputLayout_(index);
+
+        out.AlignedByteOffset    = layout.offset;
+        out.Format               = static_cast<DXGI_FORMAT>(layout.format);
+        out.InputSlot            = layout.input_slot;
+        out.InputSlotClass       = static_cast<D3D12_INPUT_CLASSIFICATION>(layout.input_slot_class);
+        out.InstanceDataStepRate = layout.instance_step_rate;
+        out.SemanticIndex        = layout.semantic_index;
+        // データが改変され、終端文字が消えても大丈夫か？
+        out.SemanticName         = getStringPtr_(layout.semantic_name_offset, layout.semantic_name_strlen);
+    }
+
+    //-------------------------------------------------------------------------
+    // ファイルフォーマット
+    // 
+    // +------------------+--------------+
+    // | Header           | 80 bytes     |
+    // +------------------+--------------+
+    // | Input layouts    | 12 bytes * N |
+    // +------------------+--------------+
+    // | Vertex buffer    | variable     |
+    // +------------------+--------------+
+    // | Index buffer     | variable     |
+    // +------------------+--------------+
+    // | Strings          | variable     |
+    // +------------------+--------------+
+    // 
+    //-------------------------------------------------------------------------
+private:
+    //! ヘッダー構造体
+    struct FileHeader
+    {
+        float    world_matrix[4 * 3] = { 0.f }; // Matrix34 は alignment を要求してくるので float に
+        uint32_t vertex_buffer_size  = 0;
+        uint32_t index_buffer_size   = 0;
+        uint16_t vertex_stride_size  = 0;
+        uint8_t  index_buffer_format = DXGI_FORMAT_R16_UINT;
+        uint8_t  primitive_topology  = D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST;
+        uint32_t mesh_name_offset    = 0;
+        uint8_t  mesh_name_strlen    = 0;
+        uint8_t  num_input_layout    = 0;
+    };
+    static_assert(sizeof(FileHeader) == 68);
+
+    //! 頂点レイアウト構造体
+    struct InputLayout
+    {
+        uint8_t  offset               = 0;
+        uint8_t  format               = DXGI_FORMAT_R32G32B32_FLOAT;
+        uint8_t  input_slot           = 0;
+        uint8_t  input_slot_class     = D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA;
+        uint8_t  instance_step_rate   = 0;
+        uint8_t  semantic_index       = 0;
+        uint8_t  semantic_name_strlen = 0;
+        uint8_t  reserved = 0;
+        uint32_t semantic_name_offset = 0;
+    };
+    static_assert(sizeof(InputLayout) == 12);
 
     //-------------------------------------------------------------------------
 private:
+    uint8_t* mPtr = nullptr;
 
+    //-------------------------------------------------------------------------
+    // internal function
+    //-------------------------------------------------------------------------
+private:
+    //! ファイルヘッダーを取得
+    const FileHeader& getFileHeader_() const
+    {
+        BEL_ASSERT(mPtr != nullptr);
+        return *reinterpret_cast<const FileHeader*>(mPtr);
+    }
+    //! 頂点レイアウトを取得
+    const InputLayout& getInputLayout_(uint32_t index) const
+    {
+        BEL_ASSERT(mPtr != nullptr);
+        BEL_ASSERT(index < getFileHeader_().num_input_layout);
+        return reinterpret_cast<const InputLayout*>(mPtr + sizeof(FileHeader))[index];
+    }
+    //! 頂点バッファーを取得
+    void* getVertexBufferPtr_() const
+    {
+        BEL_ASSERT(mPtr != nullptr);
+        const FileHeader& file_header = getFileHeader_();
+        size_t offset = sizeof(FileHeader) + sizeof(InputLayout) * file_header.num_input_layout;
+        return reinterpret_cast<void*>(mPtr + offset);
+    }
+    //! インデックスバッファーを取得
+    void* getIndexBufferPtr_() const
+    {
+        BEL_ASSERT(mPtr != nullptr);
+        const FileHeader& file_header = getFileHeader_();
+        size_t offset = sizeof(FileHeader) + sizeof(InputLayout) * file_header.num_input_layout + file_header.vertex_buffer_size;
+        return reinterpret_cast<void*>(mPtr + offset);
+    }
+    //! 文字列取得
+    const char* getStringPtr_(size_t offset, size_t) const
+    {
+        BEL_ASSERT(mPtr != nullptr);
+        return reinterpret_cast<const char*>(mPtr + offset);
+    }
 };
 
 //-----------------------------------------------------------------------------
